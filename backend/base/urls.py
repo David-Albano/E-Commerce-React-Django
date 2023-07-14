9325bf37a71486c82c51d491633737a552e30848
David Afonso Méndez <afonso27000@gmail.com>
2023-07-14 11:31:02 +0100

    Protected Routes

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('profile/', views.getUserProfile, name="users-profile"),
+    path('users/', views.getUsers, name="users"),
     path('products/', views.getProducts, name="products"),
     path('products/<str:pk>', views.getSingleProduct, name="product"),
     ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -5,7 +5,7 @@ from .models import Product
 
 from django.contrib.auth.models import User
 from .serializers import ProductsSerializer, UserSerializer, UserSerializerWithToken
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -28,6 +28,15 @@ class MyTokenObtainPairView(TokenObtainPairView):
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+
     return Response(serializer.data)
 
 
