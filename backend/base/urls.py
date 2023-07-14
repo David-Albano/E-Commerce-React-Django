dad285cd44bb768f3bc039cb6d428204946ac7bc
David Afonso Méndez <afonso27000@gmail.com>
2023-07-14 11:41:11 +0100

    Register User

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('register/', views.registerUser, name='register'),
     path('profile/', views.getUserProfile, name="users-profile"),
     path('users/', views.getUsers, name="users"),
     path('products/', views.getProducts, name="products"),
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from .models import Product
 
 from django.contrib.auth.models import User
 from .serializers import ProductsSerializer, UserSerializer, UserSerializerWithToken
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -22,6 +24,25 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail': 'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
