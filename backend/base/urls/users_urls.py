5255e46607d5f79adb54a8fa6ef861e01fda2cac
David Afonso Méndez <afonso27000@gmail.com>
2023-07-16 15:58:25 +0100

    Update Profile Endpoint

--- backend/base/urls/users_urls.py
+++ backend/base/urls/users_urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login/', users_views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register/', users_views.registerUser, name='register'),
     path('profile/', users_views.getUserProfile, name="users-profile"),
+    path('profile/update/', users_views.updateUserProfile, name="user-profile-update"),
     path('', users_views.getUsers, name="users"),
     ]
