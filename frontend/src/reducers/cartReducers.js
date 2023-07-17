8599646c5cf75c6147d9f3b729eb854d05bc50c9
David Afonso Méndez <afonso27000@gmail.com>
2023-07-17 12:43:41 +0100

    Shipping Screen & Save Address

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,9 +1,10 @@
 import {
     CART_ADD_ITEM,
-    CART_REMOVE_ITEM
+    CART_REMOVE_ITEM,
+    CART_SAVE_SHIPPING_ADDRESS
 } from '../constants/cartConstants'
 
-export const cartReducer = (state = {cartItems: [] }, action) => {
+export const cartReducer = (state = {cartItems: [], saveShippingAddressReducer: {} }, action) => {
     switch(action.type) {
         case CART_ADD_ITEM:
             const item = action.payload
@@ -27,6 +28,14 @@ export const cartReducer = (state = {cartItems: [] }, action) => {
             return {
                 ...state,
                 cartItems: state.cartItems.filter(x => x._id !== action.payload)
+            }
+        
+        // Shipping Address
+
+        case CART_SAVE_SHIPPING_ADDRESS:
+            return {
+                ...state,
+                shippingAddress: action.payload
             }
 
         default:
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -23,8 +23,15 @@ const userInfoFromStorage = localStorage.getItem('userInfo')
         ? JSON.parse(localStorage.getItem('userInfo'))
         : null
 
+const shippingInfoFromStorage = localStorage.getItem('shippingAddress') 
+        ? JSON.parse(localStorage.getItem('shippingAddress'))
+        : {}
+
 const initialState = {
-    cart: {cartItems:cartItemsFromStorage},
+    cart: {
+        cartItems:cartItemsFromStorage,
+        shippingAddress:shippingInfoFromStorage,
+    },
     userLogin: {userInfo:userInfoFromStorage},
 }
 
